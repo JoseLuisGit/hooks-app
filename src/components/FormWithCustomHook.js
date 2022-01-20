b2e6ea883755fcd88b6da0d78edd96a9fe9423e5
JoseLuisGit <joseluisstacruz@gmail.com>
2022-01-19 20:54:33 -0400

    add custom hook for the form

---
+++ src/components/FormWithCustomHook.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useForm } from '../hooks/useForm';
+
+
+export const FormWithCustomHook = () => {
+
+    const [ form, handleInputChange ] = useForm({
+        email: '',
+        name: '',
+        password: ''
+    })
+
+    const { name, email, password } = form;
+
+    const handleFormSubmit = (e)=>{
+        e.preventDefault();
+        console.log( form );
+    }
+
+    return (
+        <>
+            <h1>Form With Custom Hooks</h1>
+            <p>Hello World</p>
+            <hr />
+            <form onSubmit={ handleFormSubmit }>
+                <div className='form-group'>
+                    <input
+                        className='form-control'
+                        name='name'
+                        type='text'
+                        placeholder='Insert your name'
+                        onChange={ handleInputChange }
+                        value={ name }
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        className='form-control'
+                        name='email'
+                        type='email'
+                        placeholder='Insert your Email'
+                        onChange={ handleInputChange } 
+                        value={ email }
+                    />
+
+                </div>
+
+                <div className='form-group'>
+                    <input
+                        className='form-control'
+                        name='password'
+                        type='password'
+                        placeholder='Insert your Pass'
+                        onChange={ handleInputChange } 
+                        value={ password }
+                    />
+
+                </div>
+
+                <button type="submit" className="btn btn-success">
+                    Save
+                </button>
+
+            </form>
+
+        </>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SimpleForm } from './components/SimpleForm';
+import { FormWithCustomHook } from './components/FormWithCustomHook';
+//import { SimpleForm } from './components/SimpleForm';
 import './index.css';
 
 
 ReactDOM.render(
-    <SimpleForm />,
+    <FormWithCustomHook />,
   document.getElementById('root')
 );
 
